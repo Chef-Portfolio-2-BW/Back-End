7a7441301060ac6a154447c402ff5e8d805c6bd0
dang lu <danglu@dangs-mbp.lan>
2020-02-05 20:14:07 -0800

    removed node modules. added env

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
+require('dotenv').config();
+
 const server = require('./server.js');
 
 const PORT = process.env.PORT || 5000;
 
 if(!module.parent){
-  server.listen(PORT, () => {
+  server.listen(process.env.PORT, () => {
     console.log(`Listening on port ${PORT}...`);
   });
 }
